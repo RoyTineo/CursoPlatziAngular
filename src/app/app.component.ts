8150a783cba1a83adebf7c19c1767be429150c7b
RoyTineoGuillen <roy.tineo.27@unsch.edu.pe>
2022-11-25 10:49:26 -0500

    interpolacion y building

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-store';
-  nombre: string = "Roy"
-  edad:number = 22
-  video:string = "https://i.ytimg.com/vi/kEE6CFKGDIc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB5zlLIwFCJiprhJ7w75K9BeemxtQ"
+  nombre: string = 'Roy';
+  edad: number = 22;
+  img: string = 'https://source.unsplash.com/random';
+  btnDisable = true;
+persona ={
+    nombre: "Susan",
+    eda:21
+}
+
+toggleButton(){
+    this.btnDisable = !this.btnDisable
+}
+
+aumentarEdad(){
+    this.edad+=1
+}
+
+onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+    
+
+}
+
+cambioNombre(event:Event){
+const element =event.target as HTMLInputElement
+this.persona.nombre=element.value;
+}
 }
